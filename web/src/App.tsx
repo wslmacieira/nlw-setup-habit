b9ca706b33a18e8bb599db6c20ad5c27efc00b8d
wslmacieira <wslmacieira@outlook.com>
2023-01-23 07:29:07 -0300

    cria projeto web

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import { Habit } from "./components/Habit";
+
+import  './styles/global.css'
+
+function App() {
+  return (
+    <>
+      <Habit completed={3} />
+      <Habit completed={10}/>
+      <Habit completed={20}/>
+      <Habit completed={30}/>
+    </>
+  );
+}
+
+export default App;
