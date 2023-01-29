078e25a166afa17c4c691f95aa9f87b9c43c8677
wslmacieira <wslmacieira@outlook.com>
2023-01-29 10:44:51 -0300

    aula 2 - avançando o backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,14 @@
 import cors from '@fastify/cors';
-import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
+import { appRoutes } from './routes';
 
 const app = Fastify()
-const prisma = new PrismaClient()
+
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/hello', async () => {
-  const habits = await prisma.habit.findMany()
-  return habits
-})
+
 
 app.listen({
   port: 3333
